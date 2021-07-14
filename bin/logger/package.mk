42426c4a87cb6be7bea7a746879106f5548f4ed5
UAlbertaPsychoLing <87198497+UAlbertaPsychoLing@users.noreply.github.com>
2021-07-13 23:05:23 -0400

    Create package.mk
    Initial commit

---
+++ bin/logger/package.mk
@@ -0,0 +1,7 @@
+SRC +=  logger/Logger.java              \
+	logger/BasicLog.java            \
+        logger/Reporter.java            \
+	logger/VariableSetReporter.java \
+	logger/GuiOutputStream.java     \
+	logger/PNuanceClientLogger.java \
+	logger/PNuanceServerLog.java
