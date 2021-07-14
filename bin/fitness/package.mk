468c59312820fb6b39fa320f2cc5941c8b3597dd
UAlbertaPsychoLing <87198497+UAlbertaPsychoLing@users.noreply.github.com>
2021-07-14 11:35:54 -0400

    Create package.mk
    Initial commit

---
+++ bin/fitness/package.mk
@@ -0,0 +1,13 @@
+SRC +=  fitness/FitnessFunction.java                  \
+	fitness/AgeWeightedFitnessFunction.java       \
+	fitness/ParsimonyFitnessFunction.java         \
+	fitness/TargetFitnessFactory.java             \
+	fitness/PctHitFitnessFunction.java            \
+	fitness/CorrelationFitnessFunction.java       \
+	fitness/DistanceFitnessFunction.java          \
+	fitness/ScaledFitnessFunction.java            \
+	fitness/ScaledErrorFitness.java               \
+	fitness/ScaledPctHitFitnessFunction.java      \
+	fitness/ScaledDistanceFitnessFunction.java    \
+	fitness/IntervalFitnessFunction.java          \
+	fitness/RSquaredFitnessFunction.java
