5b426d7d019cb8a317fa9527ea06500f4a680ba8
UAlbertaPsychoLing <87198497+UAlbertaPsychoLing@users.noreply.github.com>
2021-07-13 23:04:33 -0400

    Create package.mk
    Initial commit

---
+++ bin/mode/package.mk
@@ -0,0 +1,11 @@
+SRC +=  mode/Mode.java                   \
+	mode/BasicMode.java              \
+	mode/SingletonMode.java          \
+	mode/PairwiseMode.java           \
+	mode/NWiseMode.java              \
+	mode/PNuanceServerMode.java      \
+	mode/PNuanceClientMode.java      \
+	mode/ModeGenerator.java          \
+	mode/BasicModeGenerator.java     \
+	mode/SingletonModeGenerator.java \
+	mode/PairwiseModeGenerator.java
