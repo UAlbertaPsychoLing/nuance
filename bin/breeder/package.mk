d0e9f3566a8516f07c546541c80d055c7e4f9998
UAlbertaPsychoLing <87198497+UAlbertaPsychoLing@users.noreply.github.com>
2021-07-14 12:24:51 -0400

    Create package.mk
    Initial commit

---
+++ bin/breeder/package.mk
@@ -0,0 +1,5 @@
+SRC +=  breeder/Breeder.java                    \
+	breeder/MedianPassBreeder.java          \
+	breeder/GreedyOverselectionBreeder.java \
+	breeder/MutationBreeder.java            \
+	breeder/PNuanceBreeder.java
