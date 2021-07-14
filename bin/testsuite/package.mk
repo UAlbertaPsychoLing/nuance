1215f97dc353adc9020e4d02ce205d1a4c83f936
UAlbertaPsychoLing <87198497+UAlbertaPsychoLing@users.noreply.github.com>
2021-07-13 22:53:36 -0400

    Add files via upload
    Initial commit

---
+++ bin/testsuite/package.mk
@@ -0,0 +1,19 @@
+# .java files in this package go here
+SRC +=  testsuite/SymbolicTest.java       \
+	testsuite/BasicModeTest.java      \
+	testsuite/GuiOutputTest.java      \
+	testsuite/ConfigSetTest.java      \
+	testsuite/SymbolParseTest.java    \
+	testsuite/BatchReport.java        \
+	testsuite/IntervalTest.java       \
+	testsuite/NeuralTest.java         \
+	testsuite/NeuralEvolve.java       \
+	testsuite/IteratorTest.java       \
+	testsuite/SymbolReplaceTest.java  \
+	testsuite/BatchEval.java          \
+	testsuite/ConnectionSendTest.java \
+	testsuite/ConnectionRecvTest.java \
+	testsuite/PNuanceServerTest.java  \
+	testsuite/PNuanceClientTest.java  \
+	testsuite/SynchroTest.java        \
+	testsuite/WordFilter.java
