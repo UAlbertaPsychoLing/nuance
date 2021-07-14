c098be2835bee512202dcffd19e3d66f42adce8b
UAlbertaPsychoLing <87198497+UAlbertaPsychoLing@users.noreply.github.com>
2021-07-13 22:15:56 -0400

    Create package.mk
    Initial commit

---
+++ bin/ui/gui/package.mk
@@ -0,0 +1,13 @@
+SRC += 	ui/gui/GuiNuance.java                    \
+	ui/gui/Closable.java                     \
+	ui/gui/ClosableTabbedPane.java           \
+	ui/gui/IconClosableTabbedPane.java       \
+	ui/gui/MoveSelectionPanel.java           \
+	ui/gui/SetupChooser.java                 \
+	ui/gui/ConnectSetup.java                 \
+	ui/gui/BasicSetup.java                   \
+	ui/gui/AdvancedSetup.java                \
+	ui/gui/NeuralNetSetup.java               \
+	ui/gui/Utils.java                        \
+	ui/gui/SExpressionInfoPanel.java         \
+	ui/gui/NuancePanel.java
