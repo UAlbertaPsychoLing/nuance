d177a2e4d79878de50ec5cefffd1e92053f1713b
UAlbertaPsychoLing <87198497+UAlbertaPsychoLing@users.noreply.github.com>
2021-07-14 12:28:46 -0400

    Create package.mk
    Initial commit

---
+++ bin/agent/ann/package.mk
@@ -0,0 +1,8 @@
+SRC  += agent/ann/ActivationFunction.java     \
+	agent/ann/SigmoidActivation.java      \
+	agent/ann/GaussianActivation.java     \
+	agent/ann/NeuralNode.java             \
+	agent/ann/Connection.java             \
+	agent/ann/NeuralNetwork.java          \
+	agent/ann/NeuralAgent.java            \
+	agent/ann/NeuralFactory.java
