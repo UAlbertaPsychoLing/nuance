f969d3ac036fac9c8a4a05632fef5310109e98e6
UAlbertaPsychoLing <87198497+UAlbertaPsychoLing@users.noreply.github.com>
2021-07-14 12:25:35 -0400

    Create package.mk
    Initial commit

---
+++ bin/agent/package.mk
@@ -0,0 +1,6 @@
+# the SRC files that are a part of this package
+SRC +=   agent/Agent.java             \
+	 agent/BasicAgent.java        \
+	 agent/AgentFactory.java      \
+	 agent/FitnessComparator.java \
+	 agent/AgeComparator.java
